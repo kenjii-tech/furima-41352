5404c226e9e1d20b5e67b78541c0832802376b29
kenjii-tech <techskj61@gmail.com>
2024-08-29 19:41:43 +0900

    テストコードの設定2

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,10 @@ class User < ApplicationRecord
     message: 'は全角文字（漢字・ひらがな・カタカナ）で入力してください'
   }
 
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください' }
+  validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'は全角文字（漢字・ひらがな・カタカナ）で入力してください' }
+  validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' }
+
   validates :first_name, presence: true, format: {
     with: /\A[一-龥]+\z/,
     message: 'は漢字（全角）で入力してください'
