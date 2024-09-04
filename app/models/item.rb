54d2469af8a038c4d80e536483186cc4d08882a1
kenjii-tech <techskj61@gmail.com>
2024-09-04 16:57:39 +0900

    商品ん出品機能（テストコード）

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,20 +13,15 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_day
 
   validates :name, :description, presence: true
-  validate :image_presence
+  validates :price, presence: true
+  validates :image, presence: true
   validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id,
-            numericality: { other_than: 1, message: 'を選択してください' }
+            numericality: { other_than: 1, message: "can't be blank" }
 
   validates :price, numericality: {
     greater_than_or_equal_to: 300,
     less_than_or_equal_to: 9_999_999,
     only_integer: true,
-    message: 'は¥300~¥9,999,999の間で入力してください'
+    message: 'must be between ¥300 and ¥9,999,999'
   }
-
-  private
-
-  def image_presence
-    errors.add(:image, 'を選択してください') unless image.attached?
-  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,81 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'is valid with all attributes' do
+      @item = FactoryBot.build(:item)
+      expect(@item).to be_valid
+    end
+
+    it 'is invalid without an image' do
+      @item = FactoryBot.build(:item)
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+
+      # @item = FactoryBot.build(:item, image: nil)
+      # @item.valid?
+      # expect(@item.errors.full_messages).to include("can't be blank")
+    end
+
+    it 'is invalid without a name' do
+      @item = FactoryBot.build(:item, name: nil)
+      @item.valid?
+      expect(@item.errors[:name]).to include("can't be blank")
+    end
+
+    it 'is invalid without a description' do
+      @item = FactoryBot.build(:item, description: nil)
+      @item.valid?
+      expect(@item.errors[:description]).to include("can't be blank")
+    end
+
+    it 'is invalid without a category' do
+      @item = FactoryBot.build(:item, category_id: nil)
+      @item.valid?
+      expect(@item.errors[:category_id]).to include("can't be blank")
+    end
+
+    it 'is invalid without a condition' do
+      @item = FactoryBot.build(:item, condition_id: nil)
+      @item.valid?
+      expect(@item.errors[:condition_id]).to include("can't be blank")
+    end
+
+    it 'is invalid without a shipping fee' do
+      @item = FactoryBot.build(:item, shipping_fee_id: nil)
+      @item.valid?
+      expect(@item.errors[:shipping_fee_id]).to include("can't be blank")
+    end
+
+    it 'is invalid without a prefecture' do
+      @item = FactoryBot.build(:item, prefecture_id: nil)
+      @item.valid?
+      expect(@item.errors[:prefecture_id]).to include("can't be blank")
+    end
+
+    it 'is invalid without a shipping day' do
+      @item = FactoryBot.build(:item, shipping_day_id: nil)
+      @item.valid?
+      expect(@item.errors[:shipping_day_id]).to include("can't be blank")
+    end
+
+    it 'is invalid without a price' do
+      @item = FactoryBot.build(:item, price: nil)
+      @item.valid?
+      expect(@item.errors[:price]).to include("can't be blank")
+    end
+
+    it 'is invalid with a price less than 300' do
+      @item = FactoryBot.build(:item, price: 299)
+      @item.valid?
+      expect(@item.errors[:price]).to include('must be between ¥300 and ¥9,999,999')
+    end
+
+    it 'is invalid with a price greater than 9,999,999' do
+      @item = FactoryBot.build(:item, price: 10_000_000)
+      @item.valid?
+      expect(@item.errors[:price]).to include('must be between ¥300 and ¥9,999,999')
+    end
+  end
 end
